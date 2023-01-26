91df2f742f0f977c7b128a970893c6d6d06bb6a9
szymonzalewski <szymon.marek.zalewski@gmail.com>
2023-01-26 20:07:24 +0100

    Extract CashRegister and Product into separate files

---
+++ cash-register/CashRegister.cs
@@ -0,0 +1,69 @@
+﻿namespace cash_register
+{
+    class CashRegister
+    {
+        public List<Product> Products { get; }
+        public CashRegister()
+        {
+            Products = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            Products.Remove(product);
+        }
+
+
+
+        public void RemoveProduct(int order)
+        {
+            Products.RemoveAt(order);
+        }
+
+        public double FullPrice()
+        {
+            double fullPrice = 0;
+            foreach (Product product in Products)
+            {
+                fullPrice += product.price;
+            }
+            return fullPrice;
+        }
+
+        public void Print()
+        {
+            foreach (Product product in Products)
+            {
+                product.Print();
+
+            }
+            Console.WriteLine("------------------\nFull price: {0}zł", FullPrice());
+        }
+
+        public double Payment(double cash)
+        {
+            double price = FullPrice();
+            if (price > cash)
+            {
+                throw new Exception("Not enough funds");
+            }
+            if (Products.Count == 0)
+            {//Musisz dodać produkty, zanim przejdziesz do płatności
+                throw new Exception("Before payment\t\nYou must add products");
+            }
+            //Receipt
+            Print();
+            var currentCash = cash - price;
+            Products.Clear();
+
+            return currentCash;
+
+           
+        }
+    }
+}
---
+++ cash-register/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace cash_register
+{
+    class Product
+    {
+        private static int quantity = 0; //In this case, it is only used to generate the id. it's supposed to simulate a bit as if the data came from a database
+        public int id;
+        public string name;
+        public string description;
+        public double price;
+        public Product(string name, string ingredients, double price)
+        {
+            id = quantity;
+            quantity += 1;
+            this.name = name;
+            this.description = ingredients;
+            this.price = price;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(" ID: {0}\n Name of product: {1}\n   ingredients: {2}\n   price: {3}zł", id, name, description, price);
+        }
+    }
+}
